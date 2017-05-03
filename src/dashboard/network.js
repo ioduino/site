c3c968b879ede535970a8d65dd734f057ce8c830
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2017-05-03 01:55:15 -0600

    fix other rendering issues

--- src/dashboard/network.js
+++ src/dashboard/network.js
@@ -74,43 +74,44 @@ class Network extends Component{
     };
     return (
       <div>
-      <Route path="/dashboard/network/add" exact render={() => (
-        <Box col={12} p={2}>
-          <Paper style={style.paper} zDepth={1}>
-            <Flex col={12} wrap>
-              <Box col={12}>
-                <Subheader>Adding Network</Subheader>
-              </Box>
-              <Box p={2} col={6} style={style.label}>
-                Found through the browser http://deviceip:8080/<br/><br/>
-                <Subheader>Example: "Device Network: 9c8b7089-684e-4b2c-9879-daf5c2d1918d"</Subheader>
-              </Box>
-              <Box p={2} col={6}>
-                <TextField
-                  hintText="found on device"
-                  floatingLabelText="Network Identifier"
-                  onChange={(e, text)=>{this.identifierChange(text)}}
-                  fullWidth={true}
-                />
-              </Box>
-              <Box p={2} col={6} style={style.label}>
-                Just give the network a easily identified name
-              </Box>
-              <Box p={2} col={6}>
-                <TextField
-                  hintText="My Network"
-                  floatingLabelText="Label"
-                  onChange={(e, text)=>{this.labelChange(text)}}
-                  fullWidth={true}
-                />
-              </Box>
-              <Box p={2} col={12} style={style.right}>
-                <RaisedButton label="Save Label" primary onTouchTap={()=>{this.props.network.register(this.state.label, this.state.identifier);}} />
-              </Box>
-            </Flex>
-          </Paper>
-        </Box>
-      )}/>
+        {(this.props.network.selected!="null") && (
+        <Route path="/dashboard/network/add" exact render={() => (
+          <Box col={12} p={2}>
+            <Paper style={style.paper} zDepth={1}>
+              <Flex col={12} wrap>
+                <Box col={12}>
+                  <Subheader>Adding Network</Subheader>
+                </Box>
+                <Box p={2} col={6} style={style.label}>
+                  Found through the browser http://deviceip:8080/<br/><br/>
+                  <Subheader>Example: "Device Network: 9c8b7089-684e-4b2c-9879-daf5c2d1918d"</Subheader>
+                </Box>
+                <Box p={2} col={6}>
+                  <TextField
+                    hintText="found on device"
+                    floatingLabelText="Network Identifier"
+                    onChange={(e, text)=>{this.identifierChange(text)}}
+                    fullWidth={true}
+                  />
+                </Box>
+                <Box p={2} col={6} style={style.label}>
+                  Just give the network a easily identified name
+                </Box>
+                <Box p={2} col={6}>
+                  <TextField
+                    hintText="My Network"
+                    floatingLabelText="Label"
+                    onChange={(e, text)=>{this.labelChange(text)}}
+                    fullWidth={true}
+                  />
+                </Box>
+                <Box p={2} col={12} style={style.right}>
+                  <RaisedButton label="Save Label" primary onTouchTap={()=>{this.props.network.register(this.state.label, this.state.identifier);}} />
+                </Box>
+              </Flex>
+            </Paper>
+          </Box>
+        )}/>)}
         <Route path="/dashboard" exact render={()=>(
           <Flex wrap lg={12}>
             {this.props.network.devicesArray.map((device)=>this.valve(this.props.network.devicesObjs[device], device))}
